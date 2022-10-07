5948d4fcc83e1cef6baee3f9f11651d10a1849ea
Dez <dez@MacBook-Pro-de-Dez.local>
2022-10-07 20:29:31 +0200

    Webassembly test3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/webassembly-test1'));
 app.use(express.static(__dirname + '/public/webassembly-test2'));
+app.use(express.static(__dirname + '/public/webassembly-test3'));
 
 app.get('/webassembly1', function (req, res) {
     res.sendFile(path.join(__dirname+'/public/webassembly-test1/hello.html'));
@@ -13,6 +14,10 @@ app.get('/webassembly1', function (req, res) {
 
 app.get('/webassembly2', function (req, res) {
     res.sendFile(path.join(__dirname+'/public/webassembly-test2/hello2.html'));
+})
+
+app.get('/webassembly3', function (req, res) {
+    res.sendFile(path.join(__dirname+'/public/webassembly-test3/hello3.html'));
 })
 
 app.get('/about',function(req,res){
