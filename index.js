62f6806d2f8d51af9ab69054cffbbab398500e8d
Dez <dez@MacBook-Pro-de-Dez.local>
2022-10-07 19:53:35 +0200

    Init webassembly branch

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-//app.use(express.staticProvider(__dirname + '/public'));
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-   // res.render('hello.html')
     res.sendFile(path.join(__dirname+'/public/hello.html'));
 })
 
